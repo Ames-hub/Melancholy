f65a6fb06af50385db13951a6b151c670c5c8baf
Amelia <114128556+Ames-hub@users.noreply.github.com>
2023-02-15 06:11:07 +0800

    QUICKLY FIXING EVERYTHING
    F**K PYTHON IMPORTS WHY IS IT BROKEN HERE

--- Chapters/Chapter2.py
+++
@@ -1,3 +0,0 @@
-print("Sorry! But thats the end of Melancholy. I will continue to develop it :)")
-print("Head over to my Github page if you want to check for updates!")
-print("https://github.com/Ames-Hub/Melancholy")
--- logs/DONTDELETE.md
+++
@@ -1,2 +0,0 @@
-Delete logs folder and everything fucking breaks.
-Dont delete this Readme either or Github won't save the logs folder!
